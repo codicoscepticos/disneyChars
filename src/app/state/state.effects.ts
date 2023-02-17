5ded4f5169b5d6b9e4d7ce59cba0267cebd67f0f
codicoscepticos <1800200+codicoscepticos@users.noreply.github.com>
2023-02-18 00:40:21 +0200

    use `mergeMap` instead of `switchMap`
    allows to fetch multiple pages in parallel (in case the next page button, to fetch new pages, is pressed quickly)

--- src/app/state/state.effects.ts
+++ src/app/state/state.effects.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
-import { map, catchError, switchMap } from 'rxjs/operators';
+import { map, catchError, mergeMap } from 'rxjs/operators';
 import { Page } from '../interfaces/Page';
 
 import { DisneyAPIService } from '../services/disney-api.service';
@@ -17,7 +17,7 @@ export class CharsPageEffects{
     fetchCharsPage$ = createEffect(()=>
         this.actions$.pipe(
             ofType(fetchCharsPage),
-            switchMap(
+            mergeMap(
                 ({pageIndex})=>this.disneyAPIService.getCharsPage(pageIndex).pipe(
                     map(charsPage=>succeedFetchingCharsPage({ charsPage:<Page> charsPage })),
                     catchError(error=>of(failFetchingCharsPage({ error })))
