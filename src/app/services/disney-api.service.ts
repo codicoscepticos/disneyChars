a9d207e56c611daf5335aa7478350c9414be30f9
codicoscepticos <1800200+codicoscepticos@users.noreply.github.com>
2024-03-18 21:16:03 +0200

    👽️ update due to disneyapi change
     - replace `characters` to `character`

--- src/app/services/disney-api.service.ts
+++ src/app/services/disney-api.service.ts
@@ -1,21 +1,21 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
 @Injectable()
-export class DisneyAPIService{
-  constructor(private http:HttpClient){}
-  
+export class DisneyAPIService {
+  constructor(private http: HttpClient) {}
+
   static readonly baseUrl = 'https://api.disneyapi.dev';
-  static readonly baseUrlPage = DisneyAPIService.baseUrl + '/' +'characters?page=';
-  static readonly baseUrlSearch = DisneyAPIService.baseUrl + '/' +'character?';
+  static readonly baseUrlPage = DisneyAPIService.baseUrl + '/' + 'character?page=';
+  static readonly baseUrlSearch = DisneyAPIService.baseUrl + '/' + 'character?';
   static readonly resultsNumPerPage = 50;
-  
-  getCharsPage(pageIndex:number){
+
+  getCharsPage(pageIndex: number) {
     const url = DisneyAPIService.baseUrlPage + pageIndex;
     return this.http.get(url);
   }
-  
-  getSearchCharsPage(query:string){
+
+  getSearchCharsPage(query: string) {
     const url = DisneyAPIService.baseUrlSearch + query;
     return this.http.get(url);
   }
